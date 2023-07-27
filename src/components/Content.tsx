b3db5fbe02f426611129a3414d6891ff62bf9aa8
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-07-27 12:49:14 -0600

    feat: add functionality to add and remove items from cart

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,36 +1,54 @@
-import {Button, List, ListItem} from '@ui-kitten/components';
+import {Button, List, ListItem, Icon} from '@ui-kitten/components';
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import data from '../../data.json';
 
-interface IListItem {
-  title: string;
-  id: number;
-  price: number;
-  description: string;
-}
+import {useStoreCtx} from '../context';
+import {Item} from '../models';
 
-const renderItemAccessory = (): React.ReactElement => (
-  <Button size="tiny">Add to cart</Button>
-);
+const ItemAccessory = ({item}: {item: Item}): React.ReactElement => {
+  const {addToCart, removeFromCart, cart} = useStoreCtx();
+
+  const alreadyonCart = cart.some(cartItem => cartItem.id === item.id);
+
+  const handleOnPress = () => {
+    if (alreadyonCart) {
+      removeFromCart(item);
+    } else {
+      addToCart(item);
+    }
+  };
+
+  const iconName = alreadyonCart ? 'trash-outline' : 'shopping-cart-outline';
+
+  return (
+    <Button
+      size="tiny"
+      status={alreadyonCart ? 'danger' : 'primary'}
+      accessoryLeft={props => <Icon name={iconName} {...props} />}
+      onPress={handleOnPress}
+    />
+  );
+};
 
 export const Content = () => {
+  const {items} = useStoreCtx();
+
   return (
     <View style={styles.container}>
       <List
         style={styles.container}
-        data={data}
+        data={items}
         renderItem={({
           item,
           index,
         }: {
-          item: IListItem;
+          item: Item;
           index: number;
         }): React.ReactElement => (
           <ListItem
             title={`${item.title} | $${item.price}`}
             description={`${item.description} ${index + 1}`}
-            accessoryRight={renderItemAccessory}
+            accessoryRight={<ItemAccessory item={item} />}
           />
         )}
       />
