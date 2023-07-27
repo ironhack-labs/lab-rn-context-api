71e4fa4ccff537a5736cb8b10384fe100c7edaf1
Jesus Gutierrez <jesus.gutierrez@digitalfemsa.com>
2023-07-27 17:57:33 -0500

    done

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
 
 // UI KItten
@@ -16,25 +16,37 @@ import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import {AppContextProvider, useAppContext} from './context/appContext';
+import CartScreen from './screens/CartScreen';
 
-export default () => (
-  <>
-    <SafeAreaView style={styles.topArea} />
-    <StatusBar barStyle="light-content" backgroundColor="rgb(21, 26, 48)" />
-    <SafeAreaView style={styles.mainArea}>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.dark}>
-        <Layout style={styles.mainArea}>
-          <Header />
-          <Divider />
-          <Content />
-          <Divider />
-          <Footer />
-        </Layout>
-      </ApplicationProvider>
-    </SafeAreaView>
-  </>
-);
+export default () => {
+  const [showCart, setShowCart] = useState(false);
+
+  return (
+    <AppContextProvider>
+      <SafeAreaView style={styles.topArea} />
+      <StatusBar barStyle="light-content" backgroundColor="rgb(21, 26, 48)" />
+      <SafeAreaView style={styles.mainArea}>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={eva.dark}>
+          <Layout style={styles.mainArea}>
+            {!showCart ? (
+              <>
+                <Header />
+                <Divider />
+                <Content />
+                <Divider />
+                <Footer showCart={() => setShowCart(true)} />
+              </>
+            ) : (
+              <CartScreen hideCart={() => setShowCart(false)} />
+            )}
+          </Layout>
+        </ApplicationProvider>
+      </SafeAreaView>
+    </AppContextProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   topArea: {flex: 0, backgroundColor: 'rgb(21, 26, 48)'},
