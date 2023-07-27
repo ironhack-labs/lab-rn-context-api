2611a84ca6562e82fe97b9611f0e0182a773c04a
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-07-27 15:11:09 -0700

    done

--- App.tsx
+++ App.tsx
@@ -1,49 +1,33 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppProvider } from './src/store';
+import { CartScreen, HomeScreen } from './src/screens';
 
-// UI KItten
-import * as eva from '@eva-design/eva';
-import {
-  ApplicationProvider,
-  Divider,
-  IconRegistry,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
+export type RootStackParamList = {
+  Home: undefined;
+  Cart: undefined;
+};
 
-// Components
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
+const Stack = createNativeStackNavigator();
 
-export default () => (
-  <>
-    <SafeAreaView style={styles.topArea} />
-    <StatusBar barStyle="light-content" backgroundColor="rgb(21, 26, 48)" />
-    <SafeAreaView style={styles.mainArea}>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.dark}>
-        <Layout style={styles.mainArea}>
-          <Header />
-          <Divider />
-          <Content />
-          <Divider />
-          <Footer />
-        </Layout>
-      </ApplicationProvider>
-    </SafeAreaView>
-  </>
-);
-
-const styles = StyleSheet.create({
-  topArea: {flex: 0, backgroundColor: 'rgb(21, 26, 48)'},
-  mainArea: {
-    flex: 1,
-    backgroundColor: 'rgb(21, 26, 48)',
-  },
-
-  card: {
-    flex: 1,
-  },
-});
+export default () => {
+  return (
+    <AppProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppProvider>
+  );
+};
